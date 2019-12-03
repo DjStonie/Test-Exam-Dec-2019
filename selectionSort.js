bf6923ee80f43f41fe789a74da112d0f9a4590f5
pokealai <poxx0003@stud.kea.dk>
2019-12-03 13:19:00 +0100

    selection sort is working now

--- selectionSort.js
+++ selectionSort.js
@@ -1,23 +1,25 @@
- function selectionSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-      let smallest = i;
-      /*
-      if (isNaN(arr[smallest])) {
-        if(/^[A-Za-z]+$/.test(arr[smallest])){
-          smallest = arr[smallest].charCodeAt(0)
-        } else {
-          continue;
-        }
+function selectionSort(Streng) {
+  arr = Streng.split(",");
+  for (let c = 0; c < arr.length; c++) {
+    const element = arr[c];
+    if (isNaN(arr[c])) {
+      if (/^[A-Za-z]+$/.test(arr[c])) {
+        arr[c] = arr[c].charCodeAt(0);
+      } else {
+        continue;
       }
-      */
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[j] < arr[smallest]) {
-          smallest = j;
-        }
-      }
-      const temp = arr[smallest];
-      arr[smallest] = arr[i];
-      arr[i] = temp;
     }
-    return arr;
   }
+  for (let i = 0; i < arr.length - 1; i++) {
+    let smallest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[smallest]) {
+        smallest = j;
+      }
+    }
+    const temp = arr[smallest];
+    arr[smallest] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
