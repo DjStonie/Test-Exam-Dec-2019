b786d0b0afa98982ffac23d1fa0ebd74ceadf12a
Lars-Peter Olsen <Lars.Peter.Olsen@gmail.com>
2019-12-03 15:23:09 +0100

    Fixed Test Merge Conflic

--- test/test.js
+++ test/test.js
@@ -35,11 +35,18 @@ describe('Pascal Triangle', function() {
         [1,9,36,84,126,126,84,36,9,1]]).to.eql(PascalTriangle(10));
     });
     it('Pascal Triangle Negative Value', function() {
-        assert.equal(PascalTriangle(-12),0);
+        assert.equal(PascalTriangle(-1),0);
     });
    
-    it('Pascal Triangle Negative Value', function() {
-        assert.equal(PascalTriangle(-12),0);
+    it('Pascal Triangle MaxMinusOne Value', function() {
+        expect([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1],[1,5,10,10,5,1],[1,6,15,20,15,6,1],[1,7,21,35,35,21,7,1],[1,8,28,56,70,56,28,8,1]]).to.eql(PascalTriangle(9));
     });
-   
+
+    it('Pascal Triangle ArrayStrings Value', function() {
+        assert.equal(PascalTriangle(["a","b"]),0);
+    });
+
+    it('Pascal Triangle Middle Value', function() {
+        expect([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]).to.eql(PascalTriangle(5));
+    });
 });
