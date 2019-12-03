26896b9a9094d41596d9da3e3d87f610e5d74963
DjStonie <valdonay@yahoo.dk>
2019-12-03 11:08:33 +0100

    pas

---
+++ PascalTriangle.js
@@ -0,0 +1,32 @@
+function PascalTriangle(numRows) 
+{
+	let triangle = [[1], [1,1]];
+	
+	if(numRows == 0){
+		  return[];}
+    else if(numRows === 1){
+      return[[1]];
+    }
+    else if (numRows === 2){
+      return[[1],[1, 1]];
+		}
+    else 
+    {
+      for(let i = 2; i < numRows; i++)
+      {
+			addRow(triangle);
+			}
+		}
+		return triangle;
+  }
+function addRow(triangle){
+	let previous = triangle[triangle.length - 1];
+	let newRow = [1];
+	for(let i = 0; i < previous.lenght - 1; i++){
+			let current = previous[i];
+			let next = previous[i+1];
+			newRow.push(current + next);
+		}
+	newRow.push(1);
+	return triangle.push(newRow);
+  }
