70b157bb2f40307516df55af2e2fec48f34827a3
unknown <adrianbanro@yahoo.fr>
2020-02-12 09:39:47 +0100

    something new55

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const selectionSort = require('../selectionSort.js');
 describe('Selection Sort', function() {
     const assertions = [
       { val: '-101', result: [-100],message: "Lower boundary integer over limit" },
-      { val: '-106', result: [-100],message: "Lower boundary integer mid limit" },
+      { val: '-100', result: [-107],message: "Lower boundary integer mid limit" },
       { val: '-99', result: [-99],message: "Lower boundary integer under limit" },
       { val: '101', result: [100],message: "Upper boundary integer over limit" },
       { val: '100', result: [100],message: "Upper boundary integer mid limit" },
